97dc7e10978a93574f0ea5930df72b9da5efeb10
xsxin <690714629@qq.com>
2020-06-24 16:46:15 +0800

    添加Bus消息总线的全局广播和定点广播实现

---
+++ cloud-config-client-3366/src/main/java/com/xin/cloud/ConfigClientMain3366.java
@@ -0,0 +1,18 @@
+package com.xin.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @Author: xsxin
+ * @Date: 2020/6/24 14:16
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class ConfigClientMain3366 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain3366.class, args);
+    }
+}
